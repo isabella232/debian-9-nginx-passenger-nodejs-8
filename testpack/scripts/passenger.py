cad412b32674345bf4dcb4770d02e4f4f7ddfe48
Brian Wilkinson <brian.wilkinson@fasthosts.com>
2021-04-06 11:25:25 +0100

    Removed broken/replicated passenger test

--- testpack/scripts/passenger.py
+++ testpack/scripts/passenger.py
@@ -85,10 +85,6 @@ class Test1and1Image(Test1and1Common):
         time.sleep(2)
         self.file_mode_test("/var/run/nginx.pid", "-rw-r--r--")
 
-    def test_passenger_version(self):
-        version = self.execRun("passenger --version")
-        self.assertEqual("Phusion Passenger 6", version[:19])
-
     def test_docker_logs(self):
         expected_log_lines = [
             "run-parts: executing /hooks/supervisord-pre.d/20_configurability",
